ab8a0165e631719eb89c4b12152b9ab53db998a5
rohithrajreganti <rohithrajreganti@gmail.com>
2018-06-05 11:02:55 +0530

    custom sort

--- practice.py
+++ practice.py
@@ -1,55 +1,16 @@
 from collections import defaultdict
 
+def func(a,b):
+    return 1 if a>b else -1
+
 
 def main():
-    #list demo
-    ll = list()
-    #append
-    ll.append(2)
-
-    #extend
-    ll2 = [1,2,3,4,5]
-    ll.extend(ll2)
-
-    #print
+    
+    s= "hello"
+    ll = [5,4,3,2,1]
+    ll.sort(func)
     print ll
 
-    #pop
-    print ll.pop(0)
-    
-    #count
-    print ll.count(2)
-
-    #insert
-    ll.insert(0,222)
-    print ll
-
-    """
-    dict practice
-    
-    """
-    dict1 = {3:"bye", 4:"cya"}
-    dict1[2]="hello"
-    print dict1[2]
-
-    for key,value in dict1.items():
-        print key,value
-
-    print dict1.keys()
-    print dict1.values()
-    print dict1.items()
-
-    "defualt dict"
-
-    dict2 = defaultdict(int)
-
-    for i in range(0,100):
-        dict2[i]+=1
-
-    print dict2
-    
-    print help('collections')
-    
 
 
 if __name__=="__main__":
